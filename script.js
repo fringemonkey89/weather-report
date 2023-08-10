a1fc3eb74811ebdf25440ec5af79d0043cccb198
fringemonkey89 <markomajeticjr@gmail.com>
2023-08-10 17:12:19 -0400

    added code to script file

---
+++ script.js
@@ -0,0 +1,12 @@
+const cityElement = $('enterCity');
+const searchButton = $('searchButton');
+const clearButton = $('clearHistory');
+
+
+
+
+function getWeather(){};
+
+function kelv2fer(){};
+
+function rendSearchHistory(){};
