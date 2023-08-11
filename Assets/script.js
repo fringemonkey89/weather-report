a7ea171519dab86fe8e36fe2fb779b10ea383620
fringemonkey89 <markomajeticjr@gmail.com>
2023-08-11 19:22:01 -0400

    added more code to html and javascript

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,13 @@
 const cityElement = $('enterCity');
 const searchButton = $('searchButton');
 const clearButton = $('clearHistory');
+const city_name = $('city-name');
+const currentPic = $('current-pic');
+const currentTemp = $('temperature');
+const currentHumidity = $('humidity');
+const currentWind = $('wind-speed');
 
-
+const apiKey = "1dc621126262037aadf0ef0fd44245df";
 
 
 function getWeather(){};
