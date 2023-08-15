27fd3156ef2c0527fc0fde9f87540febbbd6b045
fringemonkey89 <markomajeticjr@gmail.com>
2023-08-15 12:32:36 -0500

    linked javascript to html and added code to script file

--- Assets/script.js
+++ Assets/script.js
@@ -20,19 +20,37 @@ function getWeather(cityName){
     "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
     axios.get(queryURL)
         .then(function (response) {
-};
+            weatherToday.classList.remove('d-none');
+}
+}
 
-function kelv2fer(k){
-    return Math.floor((k - 273.15) * 1.8 + 32);
-};
-
+searchButton.addEventListener('click', function(){
+    const searchTerm = cityElement.value;
+    getWeather(searchTerm);
+    searchHistory.push(searchTerm);
+    localStorage.setItem('search', JSON.stringify(searchHistory));
+    renderSearchHistory();
+})
 clearButton.addEventListener('click', function(){
     localStorage.clear();
     searchHistory = [];
     rendSearchHistory();
 })
 
+function kelv2fer(k){
+    return Math.floor((k - 273.15) * 1.8 + 32);
+}
+
 function rendSearchHistory(){
     history.innerHTML = "";
-    
-};
+    for(let i = 0; i < searchHistory.length; i++){
+        const historyItem = $('<input>');
+        historyItem.setAttribute('type', 'text');
+        historyItem.setAttribute('readonly', true);
+        historyItem.setAttribute('class','form- control d-block bg-white');
+        historyItem.setAttribute('value', searchHistory[i]);
+    }
+
+}
+
+renderSearchHistory();
