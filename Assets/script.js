d349dc115cf3c87cea17feba8340f64afb4c2d6f
fringemonkey89 <markomajeticjr@gmail.com>
2023-08-15 11:09:04 -0400

    made further additions to script file

--- Assets/script.js
+++ Assets/script.js
@@ -6,12 +6,33 @@ const currentPic = $('current-pic');
 const currentTemp = $('temperature');
 const currentHumidity = $('humidity');
 const currentWind = $('wind-speed');
+const currentUV = $('UV-index');
+const history = $('history');
+var fiveDay = $('five-day-weather');
+var weatherToday = $('today-weather');
+let searchHistory = JSON.parse(localStorage.getItem('search')) || [];
+
 
 const apiKey = "1dc621126262037aadf0ef0fd44245df";
 
 
-function getWeather(){};
+function getWeather(cityName){
+    "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+    axios.get(queryURL)
+        .then(function (response) {
+};
 
-function kelv2fer(){};
+function kelv2fer(k){
+    return Math.floor((k - 273.15) * 1.8 + 32);
+};
 
-function rendSearchHistory(){};
+clearButton.addEventListener('click', function(){
+    localStorage.clear();
+    searchHistory = [];
+    rendSearchHistory();
+})
+
+function rendSearchHistory(){
+    history.innerHTML = "";
+    
+};
